ca606baba1dc4436f00d9bc51b83680833924eeb
chinyuting <s86052915@gmail.com>
2025-05-10 16:22:49 +0800

    add dependencies

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,38 @@
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 
+import * as monacoPlugin from 'vite-plugin-monaco-editor';
+
 export default defineConfig({
-  plugins: [vue()]
+  plugins: [
+    vue(),
+    // 假设 plugin 方法是正确的入口点
+    monacoPlugin.default.plugin ? monacoPlugin.default.plugin({
+      languageWorkers: [
+        'editorWorkerService',
+        'css',
+        'html',
+        'javascript',
+        'typescript',
+        'json'
+      ]
+    }) : undefined,
+  ],
+  resolve: {
+    alias: {
+      // 如果需要，可以添加别名
+    },
+  },
+  optimizeDeps: {
+    include: ['monaco-editor/esm/vs/editor/editor.api'],
+  },
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          'monaco-editor': ['monaco-editor'],
+        },
+      },
+    },
+  },
 });
